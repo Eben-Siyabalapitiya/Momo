e6aa14acb4626a3f386fe2193b2285e5cc494a29
Eben Siyabalapitiya <ebeneleshapen45@gmail.com>
2026-08-23 23:37:42 -0400

    Add TFT display test

---
+++ test_screen.py
@@ -0,0 +1,46 @@
+import time
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7735
+
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+spi = board.SPI()
+
+disp = st7735.ST7735R(
+    spi,
+    rotation=90,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=24000000,
+    width=128,
+    height=160,
+    x_offset=0,
+    y_offset=0,
+)
+
+width = disp.width
+height = disp.height
+
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0, 0, width, height), fill=(200, 30, 30))
+disp.image(image)
+time.sleep(1)
+
+draw.rectangle((0, 0, width, height), fill=(30, 180, 60))
+disp.image(image)
+time.sleep(1)
+
+draw.rectangle((0, 0, width, height), fill=(30, 60, 200))
+disp.image(image)
+time.sleep(1)
+
+draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
+draw.text((30, 70), "MOMO", fill=(255, 255, 255))
+disp.image(image)
