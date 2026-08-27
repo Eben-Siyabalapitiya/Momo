07b566a016db02b3eb71b620c74e569ec93b609d
Eben Siyabalapitiya <ebeneleshapen45@gmail.com>
2026-08-27 19:32:10 -0400

    Slow down gait, add standing-tall knee stance

--- gait.py
+++ gait.py
@@ -3,17 +3,24 @@ import servos
 
 HIP_SWING = 25
 KNEE_LIFT = 25
-STEP_DELAY = 0.15
+KNEE_STANCE = 105
+STEP_DELAY = 0.3
 
 LEG_ORDER = ["FR", "BL", "FL", "BR"]
 
 
+def stand_tall():
+    for leg in LEG_ORDER:
+        servos.set(leg, "knee", KNEE_STANCE)
+    time.sleep(STEP_DELAY)
+
+
 def _step_leg(leg):
-    servos.set(leg, "knee", 90 + KNEE_LIFT)
+    servos.set(leg, "knee", KNEE_STANCE + KNEE_LIFT)
     time.sleep(STEP_DELAY)
     servos.set(leg, "hip", 90 + HIP_SWING)
     time.sleep(STEP_DELAY)
-    servos.set(leg, "knee", 90)
+    servos.set(leg, "knee", KNEE_STANCE)
     time.sleep(STEP_DELAY)
 
 
@@ -27,6 +34,7 @@ def _power_stroke():
 
 
 def walk_forward(cycles=1):
+    stand_tall()
     for _ in range(cycles):
         for leg in LEG_ORDER:
             _step_leg(leg)
