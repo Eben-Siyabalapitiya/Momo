b12ef364de04372636c3a6d7a6d9d57b89d981d5
Eben Siyabalapitiya <ebeneleshapen45@gmail.com>
2026-09-02 07:30:57 +0100

    Try clearer espeak voice variant and tune amplitude

--- voice.py
+++ voice.py
@@ -96,7 +96,7 @@ def speak(text):
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as raw_file:
         raw_path = raw_file.name
 
-    subprocess.run(["espeak", "-p", "75", "-s", "165", "-w", raw_path, text])
+    subprocess.run(["espeak", "-v", "en+f3", "-p", "70", "-s", "155", "-a", "180", "-w", raw_path, text])
     subprocess.run(["aplay", "-D", "plughw:0,0", raw_path])
 
     os.remove(raw_path)
