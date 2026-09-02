6936af7143b9138068fea30af77e6a10d9c7b0a8
Castlers0550 <ebeneleshapen45@gmail.com>
2026-09-02 17:51:09 -0400

    Move wave dip legs to front-right and back-right instead of both back legs

--- gait.py
+++ gait.py
@@ -19,6 +19,12 @@ def _wave_knee_targets(home):
         return 180, 150
     return 0, 30
 
+
+def _dip_target(home, dip):
+    if home < 90:
+        return min(180, home + dip)
+    return max(0, home - dip)
+
 LEG_ORDER = ["FR", "BL", "FL", "BR"]
 
 
@@ -53,32 +59,36 @@ def walk_forward(cycles=1):
 
 def wave():
     wave_knee = servos.LEGS[WAVE_LEG]["knee"]
-    br_knee = servos.LEGS["BR"]["knee"]
-    bl_knee = servos.LEGS["BL"]["knee"]
+    wave_hip = servos.LEGS[WAVE_LEG]["hip"]
+    dip_a_knee = servos.LEGS["FR"]["knee"]
+    dip_b_knee = servos.LEGS["BR"]["knee"]
     wave_knee_home = servos.get_startup(wave_knee)
-    br_knee_home = servos.get_startup(br_knee)
-    bl_knee_home = servos.get_startup(bl_knee)
+    dip_a_home = servos.get_startup(dip_a_knee)
+    dip_b_home = servos.get_startup(dip_b_knee)
     knee_high, knee_low = _wave_knee_targets(wave_knee_home)
+
+    for leg in servos.LEGS:
+        servos.set_channel(servos.LEGS[leg]["hip"], 90)
+    time.sleep(STEP_DELAY)
 
     servos.set_channel(wave_knee, knee_high)
     time.sleep(STEP_DELAY)
-    servos.set_channel(br_knee, max(0, br_knee_home - WAVE_BACK_DIP))
-    servos.set_channel(bl_knee, min(180, bl_knee_home + WAVE_BACK_DIP))
+    servos.set_channel(dip_a_knee, _dip_target(dip_a_home, WAVE_BACK_DIP))
+    servos.set_channel(dip_b_knee, _dip_target(dip_b_home, WAVE_BACK_DIP))
     time.sleep(STEP_DELAY)
 
     for _ in range(WAVE_REPS):
         servos.set_channel(wave_knee, knee_low)
-        servos.set(WAVE_LEG, "hip", 90 - WAVE_HIP_SWING)
+        servos.set_channel(wave_hip, 90 - WAVE_HIP_SWING)
         time.sleep(WAVE_STEP_DELAY)
         servos.set_channel(wave_knee, knee_high)
-        servos.set(WAVE_LEG, "hip", 90 + WAVE_HIP_SWING)
+        servos.set_channel(wave_hip, 90 + WAVE_HIP_SWING)
         time.sleep(WAVE_STEP_DELAY)
 
-    servos.set(WAVE_LEG, "hip", 90)
+    servos.set_channel(wave_hip, 90)
     servos.set_channel(wave_knee, wave_knee_home)
-    servos.set_channel(br_knee, br_knee_home)
-    servos.set_channel(bl_knee, bl_knee_home)
-    time.sleep(STEP_DELAY)
+    servos.set_channel(dip_a_knee, dip_a_home)
+    servos.set_channel(dip_b_knee, dip_b_home)
 
 
 if __name__ == "__main__":
