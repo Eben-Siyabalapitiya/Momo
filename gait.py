8e770bbad13168ce6635af7c63c1c59a6b21a92a
Castlers0550 <ebeneleshapen45@gmail.com>
2026-09-02 17:36:37 -0400

    Switch wave to front-left leg, generalize knee target direction per leg home

--- gait.py
+++ gait.py
@@ -7,12 +7,17 @@ KNEE_LIFT = 60
 KNEE_STANCE = 0
 STEP_DELAY = 0.18
 
-WAVE_KNEE_HIGH = 180
-WAVE_KNEE_LOW = 150
+WAVE_LEG = "FL"
 WAVE_BACK_DIP = 70
 WAVE_HIP_SWING = 40
 WAVE_REPS = 5
 WAVE_STEP_DELAY = 0.15
+
+
+def _wave_knee_targets(home):
+    if home < 90:
+        return 180, 150
+    return 0, 30
 
 LEG_ORDER = ["FR", "BL", "FL", "BR"]
 
@@ -47,29 +52,30 @@ def walk_forward(cycles=1):
 
 
 def wave():
-    fr_knee = servos.LEGS["FR"]["knee"]
+    wave_knee = servos.LEGS[WAVE_LEG]["knee"]
     br_knee = servos.LEGS["BR"]["knee"]
     bl_knee = servos.LEGS["BL"]["knee"]
-    fr_knee_home = servos.get_startup(fr_knee)
+    wave_knee_home = servos.get_startup(wave_knee)
     br_knee_home = servos.get_startup(br_knee)
     bl_knee_home = servos.get_startup(bl_knee)
+    knee_high, knee_low = _wave_knee_targets(wave_knee_home)
 
-    servos.set_channel(fr_knee, WAVE_KNEE_HIGH)
+    servos.set_channel(wave_knee, knee_high)
     time.sleep(STEP_DELAY)
     servos.set_channel(br_knee, max(0, br_knee_home - WAVE_BACK_DIP))
     servos.set_channel(bl_knee, min(180, bl_knee_home + WAVE_BACK_DIP))
     time.sleep(STEP_DELAY)
 
     for _ in range(WAVE_REPS):
-        servos.set_channel(fr_knee, WAVE_KNEE_LOW)
-        servos.set("FR", "hip", 90 - WAVE_HIP_SWING)
+        servos.set_channel(wave_knee, knee_low)
+        servos.set(WAVE_LEG, "hip", 90 - WAVE_HIP_SWING)
         time.sleep(WAVE_STEP_DELAY)
-        servos.set_channel(fr_knee, WAVE_KNEE_HIGH)
-        servos.set("FR", "hip", 90 + WAVE_HIP_SWING)
+        servos.set_channel(wave_knee, knee_high)
+        servos.set(WAVE_LEG, "hip", 90 + WAVE_HIP_SWING)
         time.sleep(WAVE_STEP_DELAY)
 
-    servos.set("FR", "hip", 90)
-    servos.set_channel(fr_knee, fr_knee_home)
+    servos.set(WAVE_LEG, "hip", 90)
+    servos.set_channel(wave_knee, wave_knee_home)
     servos.set_channel(br_knee, br_knee_home)
     servos.set_channel(bl_knee, bl_knee_home)
     time.sleep(STEP_DELAY)
